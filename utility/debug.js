a58f2fb391a4249ac455307b3e3e67d554564099
cal2dev <dev.here2@gmail.com>
2018-04-02 02:03:24 +0530

    error handling implemented

---
+++ utility/debug.js
@@ -0,0 +1,34 @@
+const config = require('./../server-config');
+let debug = {};
+debug.report = function (err, req, res, next) {
+    // Do logging and user-friendly error message display    
+    //res.redirect('/public/500.html')  
+    if (res.status == 500) {
+        console.log(err.message);
+        let options = {
+            headers: {
+                'err-msg': err.message,
+                'err-info': err
+            }
+          };
+        res.sendFile(config.server.public_dir + '/html/errors/error-500.html',options);
+        //res.redirect('/public/error-500.html')  
+    }
+    //res.json({ code:res.status, message: err.message });
+};
+module.exports = debug;
+/*
+200- OK; Standard response for successful HTTP requests
+201- Created; Request has been fulfilled. New resource created
+204- No Content; Request processed. No content returned
+301- Moved Permanently; This and all future requests directed to the given URI
+304- Not Modified; Resource has not been modified since last requested
+400- Bad Request; Request cannot be fulfilled due to bad syntax
+401- Unauthorized; Authentication is possible, but has failed
+403- Forbidden; Server refuses to respond to request
+404- Not Found; Requested resource could not be found
+500- Internal Server Error; Generic error message when server fails
+501- Not Implemented; Server does not recognize method or lacks ability to fulfill
+503- Service Unavailable; Server is currently unavailable
+
+*/
